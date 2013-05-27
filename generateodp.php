0e728e846686843bff845e9ca4276c81de92887d
juanan <juanan.pereira@ehu.es>
2013-05-27 21:03:31 +0200

    Added support for PNG pictures

--- generateodp.php
+++ generateodp.php
@@ -2,28 +2,40 @@
 
 $manifestHeader = '<?xml version="1.0" encoding="UTF-8"?>
 <manifest:manifest xmlns:manifest="urn:oasis:names:tc:opendocument:xmlns:manifest:1.0" manifest:version="1.2">
- <manifest:file-entry manifest:full-path="/" manifest:version="1.2" manifest:media-type="application/vnd.oasis.opendocument.presentation"/>
- <manifest:file-entry manifest:full-path="meta.xml" manifest:media-type="text/xml"/>';
+<manifest:file-entry manifest:full-path="/" manifest:version="1.2" manifest:media-type="application/vnd.oasis.opendocument.presentation"/>
+<manifest:file-entry manifest:full-path="meta.xml" manifest:media-type="text/xml"/>';
 $manifestFooter = '<manifest:file-entry manifest:full-path="settings.xml" manifest:media-type="text/xml"/>
- <manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>
- <manifest:file-entry manifest:full-path="Thumbnails/thumbnail.png" manifest:media-type="image/png"/>
- <manifest:file-entry manifest:full-path="Configurations2/accelerator/current.xml" manifest:media-type=""/>
- <manifest:file-entry manifest:full-path="Configurations2/" manifest:media-type="application/vnd.sun.xml.ui.configuration"/>
- <manifest:file-entry manifest:full-path="styles.xml" manifest:media-type="text/xml"/>
+<manifest:file-entry manifest:full-path="content.xml" manifest:media-type="text/xml"/>
+<manifest:file-entry manifest:full-path="Thumbnails/thumbnail.png" manifest:media-type="image/png"/>
+<manifest:file-entry manifest:full-path="Configurations2/accelerator/current.xml" manifest:media-type=""/>
+<manifest:file-entry manifest:full-path="Configurations2/" manifest:media-type="application/vnd.sun.xml.ui.configuration"/>
+<manifest:file-entry manifest:full-path="styles.xml" manifest:media-type="text/xml"/>
 </manifest:manifest>';
 
+$destination = "template/Pictures/";
+
+if (is_dir("template"))
+exec("rm -rf template");
+
+exec("unzip cleantemplate.zip") or die("Can't unzip cleantemplate.zip");
 
 if ($handle = opendir($argv[1])) {
-	echo "copying jpeg picture form $argv[1] to template/Pictures :\n";
+	echo "copying pictures from $argv[1] to template/Pictures :\n";
+
+	if (!is_dir($destination))
+		mkdir($destination);
 
 	$picNum = 0;
 	while (false !== ($entry = readdir($handle))) {
-		if (!is_dir($entry) && preg_match ("/\.(jpeg|jpg)$/i", $entry))
+		if (!is_dir($entry) && preg_match ("/\.(jpeg|jpg|png)$/i", $entry))
 		{
-			$picNum++;    
-			$destination = "template/Pictures/".$picNum.".jpg";
-			echo $destination . "\n";
-			copy($argv[1]."/".$entry, $destination);
+
+			$img = getimagesize($argv[1] . "/" . $entry);
+			$fileExt = image_type_to_extension($img[2]);
+			$picNum++;
+			$destImg = $picNum.$fileExt;
+			echo $destination . $destImg . "\n";
+			copy($argv[1]."/".$entry, $destination . $destImg);
 		}
 	}
 
@@ -50,8 +62,8 @@ if ($handle = opendir($argv[1])) {
 			$patterns[0] = "/%PAGENUM%/";
 			$patterns[1] = "/%PICTURENAME%/";
 			$replacements[0] = "$i";
-			$replacements[1] = "$i.jpg";
-		 	$manifest[] = '<manifest:file-entry manifest:full-path="Pictures/' . $i . '.jpg" manifest:media-type=""/>' . "\n";
+			$replacements[1] = $i.$fileExt;
+			$manifest[] = '<manifest:file-entry manifest:full-path="Pictures/' . $i . $fileExt . '" manifest:media-type=""/>' . "\n";
 
 			$pictures[] = preg_replace($patterns,$replacements, $pictureContent);
 
@@ -59,7 +71,7 @@ if ($handle = opendir($argv[1])) {
 
 		// generate content.xml
 		file_put_contents($templateContent, $headerLines, FILE_APPEND);
-		file_put_contents($templateContent, $pictures, FILE_APPEND);	
+		file_put_contents($templateContent, $pictures, FILE_APPEND);
 		file_put_contents($templateContent, $footerLines ,FILE_APPEND);
 
 		// generate manifest.xml
@@ -68,4 +80,5 @@ if ($handle = opendir($argv[1])) {
 		file_put_contents($templateManifest, $manifestFooter, FILE_APPEND);
 
 	}
+	exec("(cd template ; zip -9 -r ../slideck.odp .)");
 }
